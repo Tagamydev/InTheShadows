0bd3c845a8401d59c54822c2e08a125cdf3c8fe4
smsanchez <smsanchez@typsa.es>
2025-05-13 14:06:31 +0200

    fix(movement and rotations)

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -31,35 +31,59 @@ func rotate_vertical(mouse_delta):
 	#calculate_rot()
 	
 func rotate_horizontal(mouse_delta):
-	var rotation_speed := 0.01
+	var rotation_speed := 0.05
 	if mouse_delta.x > 0:
 		obj.rotate_y(1 * rotation_speed)
 	if mouse_delta.x < 0:
 		obj.rotate_y(-1 * rotation_speed)
 
+func _ray_cast(event):
+	var camera = get_viewport().get_camera_3d()
+	var from = $Camera3D.position
+	var to   = from + camera.project_ray_normal(event.position) * 1000
+
+	var space_state = get_world_3d().direct_space_state
+	var ray = PhysicsRayQueryParameters3D.create(from, to)
+	var result = space_state.intersect_ray(ray)
+	return result;
+	
+
 func _input(event):
 	# Handle mouse button presses
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			# Cast a ray from the camera through the mouse cursor
-			var camera = get_viewport().get_camera_3d()
-			var from = $Camera3D.position
-			var to   = from + camera.project_ray_normal(event.position) * 1000
-
-			var space_state = get_world_3d().direct_space_state
-			var ray = PhysicsRayQueryParameters3D.create(from, to)
-			var result = space_state.intersect_ray(ray)
-			
-
-			if result.size() != 0:
-				obj = result.collider
-				normal = result.normal
-				rotating = true
+	if event is InputEventMouseButton and \
+	(event.button_index == MOUSE_BUTTON_WHEEL_DOWN or \
+	event.button_index == MOUSE_BUTTON_WHEEL_UP or \
+	event.button_index == MOUSE_BUTTON_LEFT):
+		var result = _ray_cast(event)
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				# Cast a ray from the camera through the mouse cursor
+				if result.size() != 0:
+					obj = result.collider
+					obj = obj.get_parent()
+					obj = obj.get_parent()
+					normal = result.normal
+					rotating = true
+			else:
+				last_mouse_pos = Vector2.ZERO
+				obj = null
+				normal = null
+				rotating = false
 		else:
-			last_mouse_pos = Vector2.ZERO
-			obj = null
-			normal = null
-			rotating = false
+			if event.pressed:
+				# Cast a ray from the camera through the mouse cursor
+				if result.size() != 0:
+					obj = result.collider
+					obj = obj.get_parent()
+					obj = obj.get_parent()
+					if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+						obj.rotate_z(-1 * 0.1)
+					else:
+						obj.rotate_z(1 * 0.1)
+
+	elif event is InputEventMouseButton and (event.button_index == MOUSE_BUTTON_WHEEL_DOWN or event.button_index == MOUSE_BUTTON_WHEEL_UP):
+		if event.pressed:
+			print("wheeelllll")
 
 	# Handle mouse motion when in “rotating” mode
 	elif event is InputEventMouseMotion and rotating:
